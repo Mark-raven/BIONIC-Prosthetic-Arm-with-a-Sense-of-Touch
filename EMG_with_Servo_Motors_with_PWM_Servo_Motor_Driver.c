7f7954c2e6be8417c283803add4704b80011feaf
Mark-raven <67853683+Mark-raven@users.noreply.github.com>
2023-11-01 13:24:53 +0530

    Update EMG_with_Servo_Motors_with_PWM_Servo_Motor_Driver.c

--- EMG_with_Servo_Motors_with_PWM_Servo_Motor_Driver.c
+++ EMG_with_Servo_Motors_with_PWM_Servo_Motor_Driver.c
@@ -1,1 +1,96 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+#define SAMPLE_RATE 500
+#define BAUD_RATE 115200
+#define INPUT_PIN A0
+#define BUFFER_SIZE 128
+#define SERVO_MIN 150
+#define SERVO_MAX 600
+#define DELAY_TIME 1000
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
+int circular_buffer[BUFFER_SIZE];
+int data_index, sum;
+void setup() {
+// Serial connection begin
+Serial.begin(BAUD_RATE);
+pwm.begin();
+pwm.setPWMFreq(50);
+}
+void loop() {
+// Calculate elapsed time
+static unsigned long past = 0;
+unsigned long present = micros();
+unsigned long interval = present - past;
+past = present;
+// Run timer
+static long timer = 0;
+timer -= interval;
+// Sample and get envelop
+if(timer < 0) {
+timer += 1000000 / SAMPLE_RATE;
+  
+int sensor_value = analogRead(INPUT_PIN);
+int signal = EMGFilter(sensor_value);
+int envelop = getEnvelop(abs(signal));
+Serial.print(signal);
+Serial.print(",");
+Serial.println(envelop);
+if(signal || envelop > 100){
+// Set all servos to minimum position
+for (int i = 0; i < 5; i++) {
+pwm.setPWM(i, 0, SERVO_MIN);
+}
+delay(DELAY_TIME);
+// Set all servos to maximum position
+for (int i = 0; i < 5; i++) {
+pwm.setPWM(i, 0, SERVO_MAX);
+}
+delay(DELAY_TIME);
+}
+}
+}
+// Envelop detection algorithm
+int getEnvelop(int abs_emg){
+sum -= circular_buffer[data_index];
+sum += abs_emg;
+circular_buffer[data_index] = abs_emg;
+data_index = (data_index + 1) % BUFFER_SIZE;
+return (sum/BUFFER_SIZE) * 2;
+}
+// Band-Pass Butterworth IIR digital filter, generated using filter_gen.py.
+// Sampling rate: 500.0 Hz, frequency: [74.5, 149.5] Hz.
+// Filter is order 4, implemented as second-order sections (biquads).
+float EMGFilter(float input)
+{
+float output = input;
+{
+static float z1, z2; // filter section state
 
+float x = output - 0.05159732*z1 - 0.36347401*z2;
+output = 0.01856301*x + 0.03712602*z1 + 0.01856301*z2;
+z2 = z1;
+z1 = x;
+}
+{
+static float z1, z2; // filter section state
+float x = output - -0.53945795*z1 - 0.39764934*z2;
+output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+z2 = z1;
+z1 = x;
+}
+{
+static float z1, z2; // filter section state
+float x = output - 0.47319594*z1 - 0.70744137*z2;
+output = 1.00000000*x + 2.00000000*z1 + 1.00000000*z2;
+z2 = z1;
+z1 = x;
+}
+{
+static float z1, z2; // filter section state
+float x = output - -1.00211112*z1 - 0.74520226*z2;
+output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+z2 = z1;
+z1 = x;
+}
+return output;
+}
